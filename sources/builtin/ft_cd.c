920c0d84f669240bedb5e11cfd070e1c2f2139b0
thlibers <thlibers@student.42lehavre.fr>
2026-02-05 15:12:38 +0100

    add cd exit_code

--- sources/builtin/ft_cd.c
+++ sources/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: thlibers <thlibers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 10:47:36 by nclavel           #+#    #+#             */
-/*   Updated: 2026/02/05 13:45:11 by thlibers         ###   ########.fr       */
+/*   Updated: 2026/02/05 15:10:02 by thlibers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,31 @@ static char	*parsing_dir(t_minishell *minishell, char *dir)
 		return (NULL);
 	chdir(new_path);
 	if (errno == ENAMETOOLONG)
-		printf("cd: Path is too long");
+		ft_fprintf(2, "cd: Path is too long");
 	else if (errno == ENOENT)
-		ft_fprintf(STDERR_FILENO, "cd: %s: No such file or directory\n", new_path);
+		ft_fprintf(2, "cd: %s: No such file or directory\n", new_path);
 	else if (errno == ENOTDIR)
-		printf("cd: '%s' is not a directory\n", dir);
+		ft_fprintf(2, "cd: '%s' is not a directory\n", dir);
 	else if (errno == EACCES)
-		printf("cd: Permission denied: '%s'\n", dir);
+		ft_fprintf(2, "cd: Permission denied: '%s'\n", dir);
 	else if (errno == ENOENT)
-		printf("cd: The directory '%s' does not exist\n", dir);
+		ft_fprintf(2, "cd: The directory '%s' does not exist\n", dir);
 	else if (errno == ELOOP)
-		printf("cd: Too many lenvels of symbolic links\n");
+		ft_fprintf(2, "cd: Too many lenvels of symbolic links\n");
 	else
 		edit_env(&minishell->env, "PWD", new_path);
-	 if (new_path)
-	 	free(new_path);
-	return (NULL);
+	if (new_path)
+		free(new_path);
+	if (errno > 0)
+		return (minishell->exit_code = 1, NULL);
+	return (minishell->exit_code = 0, NULL);
 }
 
 static char	*init_newpwd(t_minishell *minishell, t_command *com_arg)
 {
 	char	*pwd;
 	char	*new_pwd;
-	
+
 	if (com_arg->arguments[0][0] == '/')
 		new_pwd = ft_strdup(com_arg->arguments[0]);
 	else
@@ -56,9 +58,9 @@ static char	*init_newpwd(t_minishell *minishell, t_command *com_arg)
 	return (new_pwd);
 }
 
-static int dot_skip(char *new_pwd, int i)
+static int	dot_skip(char *new_pwd, int i)
 {
-	while (new_pwd[i] && ft_strncmp(&new_pwd[i],".." , 2) != 0)
+	while (new_pwd[i] && ft_strncmp(&new_pwd[i], "..", 2) != 0)
 		i++;
 	while (new_pwd[i] && new_pwd[i] == '.' && new_pwd[i] != '/')
 		i++;
@@ -67,21 +69,21 @@ static int dot_skip(char *new_pwd, int i)
 	return (i);
 }
 
-static char *ft_dotdot(t_minishell *minishell, t_command *com_arg)
+static char	*ft_dotdot(t_minishell *minishell, t_command *com_arg)
 {
 	int		i;
 	int		oldf_pos;
-	int 	count;
+	int		count;
 	char	*new_pwd;
-	
+
 	new_pwd = init_newpwd(minishell, com_arg);
 	count = dotcount(new_pwd);
 	while (count > 0)
 	{
 		i = 0;
-		while(new_pwd[i] && ft_strncmp(&new_pwd[i + 1],".." , 2) != 0)
+		while (new_pwd[i] && ft_strncmp(&new_pwd[i + 1], "..", 2) != 0)
 			i++;
-		while(new_pwd[i - 1] != '/')
+		while (new_pwd[i - 1] != '/')
 			i--;
 		oldf_pos = i;
 		i = dot_skip(new_pwd, i);
@@ -100,21 +102,24 @@ void	ft_cd(t_minishell *minishell, t_command *com_arg)
 	char	*updated_pwd;
 
 	arg_len = ft_strlen(com_arg->arguments[0]);
-	if(com_arg->arg_count == 0 || strcmp(com_arg->arguments[0], "~") == 0)			// "cd ~/Exemple" a gerer (expand)
+	if (com_arg->arg_count == 0 || strcmp(com_arg->arguments[0], "~") == 0)
 		parsing_dir(minishell, ft_getenv(minishell->env, "HOME"));
 	if (com_arg->arg_count > 0)
 	{
-		if(com_arg->arg_count > 0 && com_arg->arguments[0][arg_len - 1] == '/' && arg_len > 1)
+		if (com_arg->arg_count > 0 && com_arg->arguments[0][arg_len - 1] == '/'
+			&& arg_len > 1)
 			com_arg->arguments[0][arg_len - 1] = '\0';
-		if(strcmp(com_arg->arguments[0], "-") == 0)
+		if (strcmp(com_arg->arguments[0], "-") == 0)
 			parsing_dir(minishell, ft_getenv(minishell->env, "OLDPWD"));
-		else if(ft_strnstr(com_arg->arguments[0], "..", arg_len))
+		else if (ft_strnstr(com_arg->arguments[0], "..", arg_len))
 		{
 			updated_pwd = ft_dotdot(minishell, com_arg);
 			parsing_dir(minishell, updated_pwd);
 			free(updated_pwd);
 		}
 		else
-			parsing_dir(minishell, com_arg->arguments[0]);	
+			parsing_dir(minishell, com_arg->arguments[0]);
 	}
 }
+
+// "cd ~/Exemple" a gerer (expand)
