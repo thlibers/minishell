3b2950e6bc6e07683b4fd96a37188e2a3bf8299e
Gun8hoot <gun8hoot@proton.me>
2026-02-05 17:52:40 +0100

    add AST

--- sources/parsing/AST.c
+++ sources/parsing/AST.c
@@ -12,7 +12,37 @@
 
 #include "includes/minishell.h"
 
+bool  is_command(t_tok *tok)
+{
+	if (!(*tok)->prev && (*tok)->prev->type != T_WORD)
+		return (true);
+	return (false);
+}
+
+bool  is_argument(t_tok *tok)
+{
+	if ((*tok)->prev->type == T_WORD)
+		return (true);
+	return (false);
+}
+
+bool  is_file(t_tok *tok)
+{
+	if ((*tok)->prev->type != T_WORD || (*tok)->prev->type != T_FILE)
+		return (true);
+	return (false);
+}
+/*
 t_ast *create_branch(t_tok *tok)
+{
+	t_ast *ast;
+
+	ast = calloc(1, sizeof(t_ast));
+	ast->type = tok->
+}
+
+t_ast *create_tree(t_tok *tok)
 {
 
 }
+*/
