348a1ef51de1db937a938d6c6cb2f73524d3f7be
thlibers <thlibers@student.42lehavre.fr>
2026-02-06 17:02:01 +0100

    start expand

--- sources/parsing/expand.c
+++ sources/parsing/expand.c
@@ -6,21 +6,24 @@
 /*   By: thlibers <thlibers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:46:00 by thlibers          #+#    #+#             */
-/*   Updated: 2026/02/06 16:51:13 by thlibers         ###   ########.fr       */
+/*   Updated: 2026/02/06 17:00:56 by thlibers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-void	ft_expand(t_env *env)
+void	ft_expand(t_minishell *minshell)
 {
-	while (env)
+	while (minishell->)
 	{
+		
 		
 	}
 }
 
-// echo $PWD	-->		/home/.....
-// echo '$PWD'	-->		$PWD
-// echo "$PWD"	-->		/home/.....
-// echo ~		-->		/home/.....
+// echo $PWD		-->		/home/.....
+// echo '$PWD'		-->		$PWD
+// echo "$PWD"		-->		/home/.....
+// echo ~			-->		/home/.....
+// echo "'$PWD'"	-->		/home/.....
+// echo '"$PWD"'	-->		"$PWD"
